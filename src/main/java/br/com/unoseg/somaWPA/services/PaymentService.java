220e8c065a18a8f73354a45123c12b04275e2630
walter <walter0paulo@hotmail.com>
2021-10-12 15:15:30 -0300

    Added test method - confirmPaymentaccount

---
+++ src/main/java/br/com/unoseg/somaWPA/services/PaymentService.java
@@ -0,0 +1,12 @@
+package br.com.unoseg.somaWPA.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+
+    public boolean confirmPaymentaccount(String account) {
+        return true;
+    }
+}
--- src/test/java/br/com/unoseg/somaWPA/models/PaymentServiceTest.java
+++ src/test/java/br/com/unoseg/somaWPA/models/PaymentServiceTest.java
@@ -1,16 +1,38 @@
 package br.com.unoseg.somaWPA.models;
 
+import br.com.unoseg.somaWPA.services.PaymentService;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class PaymentServiceTest {
+
+    @TestConfiguration
+    static class PaymentServiceTestConfiguretion{
+        @Bean
+        static PaymentService paymentService(){
+            return new PaymentService();
+        }
+
+    }
 
     @Autowired
     PaymentService paymentService;
 
-    
+    @Test
+    public void confirmPayment_test(){
+        String account = "TendTud";
+        Boolean paymentStatus = paymentService.confirmPaymentaccount(account);
+        Assert.assertEquals(paymentStatus, true);
+    }
+
 }
